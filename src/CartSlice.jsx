47fadeabc48cb3ea67c2bb6ae990b42b78da5aab
Irenio Luis Chagua Aduviri <ichagua@gmail.com>
2024-12-09 09:38:41 -0500

    initial commit

---
+++ src/CartSlice.jsx
@@ -0,0 +1,42 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const CartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [], // Initialize items as an empty array
+    totalCantidad: 0,
+    totalCosto: 0,
+  },
+  reducers: {
+    addItem: (state, action) => {
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
+    },
+    removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload);
+    },
+    increaseQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity + 1;
+      }
+    },
+    decreaseQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity - 1;
+      }
+    },
+  },
+});
+
+export const { addItem, removeItem, increaseQuantity, decreaseQuantity} = CartSlice.actions;
+
+export default CartSlice.reducer;
